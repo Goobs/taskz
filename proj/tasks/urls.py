291f3a31871e10b32de7cb2210ec3562057ab17e
Goobs <goobs.onm@gmail.com>
2014-01-05 15:35:39 +0400

    html prototypes user and feed

--- proj/tasks/urls.py
+++ proj/tasks/urls.py
@@ -3,6 +3,9 @@
 from django.conf.urls import patterns, url
 from .views import *
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     (r'^$', TemplateView.as_view(template_name='index.html')),
+    (r'^feed/?$', TemplateView.as_view(template_name='proto/feed.html')),
+    (r'^user/?$', TemplateView.as_view(template_name='proto/user.html')),
 )
