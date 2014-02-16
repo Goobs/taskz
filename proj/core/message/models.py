31de21a61ba34882fa30ba0ebd3ae8294e02537f
Goobs <goobs@mail.ru>
2014-02-16 20:07:40 +0400

    rename field

--- proj/core/message/models.py
+++ proj/core/message/models.py
@@ -7,7 +7,7 @@ from .managers import *
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', verbose_name=u'Отправитель')
-    recipient = models.ForeignKey(User, related_name='received_messages', verbose_name=u'Получатель')
+    recepient = models.ForeignKey(User, related_name='received_messages', verbose_name=u'Получатель')
     message = models.CharField(max_length=1024, verbose_name=u'Сообщение')
     date = models.DateTimeField(auto_now_add=True, verbose_name=u'Отправлено')
     read = models.BooleanField(default=False, verbose_name=u'Прочитано')
@@ -19,4 +19,4 @@ class Message(models.Model):
         verbose_name_plural = u'Сообщения'
 
     def __unicode__(self):
-        return '%s -> %s %s' % (self.sender, self.recipient, self.date)
+        return '%s -> %s %s' % (self.sender, self.recepient, self.date)
